2bebe26263d312d4aeea88e3a3cf4941dcea016d
alinachroma <alinachroma@gmail.com>
2018-10-21 01:21:08 +0200

    Upd

---
+++ Java Syntax/Level05/task0404/src/task0404/Cat.java
@@ -0,0 +1,15 @@
+package task0404;
+
+public class Cat {
+	
+	    private static int catsCount = 0;
+
+	    public static void addNewCat() {
+	    	Cat.catsCount += 1;    	
+	    }
+
+	    public static void main(String[] args) {
+
+	    }
+	}
+
