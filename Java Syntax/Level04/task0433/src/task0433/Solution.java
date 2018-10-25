2194952b1a66c7533b8c6b7f0e2edff0c2959859
alinachroma <alinachroma@gmail.com>
2018-10-25 14:22:00 +0200

    Completed

--- Java Syntax/Level04/task0433/src/task0433/Solution.java
+++ Java Syntax/Level04/task0433/src/task0433/Solution.java
@@ -1,5 +1,22 @@
 package task0433;
+import java.io.*;
+
 
 public class Solution {
-
-} 
+	
+    public static void main(String[] args) throws Exception {
+       run();
+    }
+    
+    public static void run() throws Exception {
+    	
+    	String toPrint = "SSSSSSSSSS";
+    	int i = 0;
+    	
+    	while (i < 10) {
+    		
+    		i++;
+    		System.out.println(toPrint);
+    	} 		
+    }
+}
