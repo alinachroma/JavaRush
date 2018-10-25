f228b7abdb60d60a3f97940c76bda39c3b26dcfd
alinachroma <alinachroma@gmail.com>
2018-10-25 19:38:30 +0200

    Upd

---
+++ Java Syntax/Level04/task0434/src/task0434/Solution.java
@@ -0,0 +1,39 @@
+package task0434;
+import java.io.*;
+
+/* 
+The multiplication table
+*/
+
+
+public class Solution {
+	
+    public static void main(String[] args) throws Exception {
+        run();
+    }
+    
+    public static void run() {
+    	
+    	int m = 0;
+    	
+    	while (m < 10) {
+    	  m++;
+    	  int n = 0; 
+    	  
+    	  while (n < 10) {
+    		n++;
+    		int mn = n*m;
+            System.out.print(mn + " ");
+    	   }	
+    	      System.out.println();
+       }
+    	
+    }
+}
+    	
+
+
+
+
+    
+
