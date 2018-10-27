46207c0734eeb153527a3e73add8a50ec9bd9235
alinachroma <alinachroma@gmail.com>
2018-10-27 13:11:00 +0200

    Upd

---
+++ Java Syntax/Level04/task0435/src/task0435/Solution.java
@@ -0,0 +1,22 @@
+package task0435;
+
+/* 
+Even numbers (1 to 100)
+*/
+
+public class Solution {
+	
+    public static void main(String[] args) throws Exception {
+    
+    	for (int i = 1; i <= 100; i++) {
+    		if (i % 2 == 0) 
+    		{
+    			System.out.println(i);
+    		}
+    	}
+    }
+}
+
+    
+    
+    
