bb5c6b3e1a2ef21f10761409f93e34b7499277b3
alinachroma <alinachroma@gmail.com>
2018-10-27 19:33:26 +0200

    Upd

--- Java Syntax/Level04/task0437/src/task0437/Solution.java
+++ Java Syntax/Level04/task0437/src/task0437/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
     public static void main(String[] args) throws Exception{
     	
     	for (int x = 1; x < 11; x++) {
-    		for (int y = 0; y < x; y = y + 1) {
+    		for (int y = 0; y < x; y++) {
     			System.out.print('8');
     		}
     		System.out.println();
