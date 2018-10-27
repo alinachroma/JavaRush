ede9de54e609b6996b4811938e69f5ec4d394ad6
alinachroma <alinachroma@gmail.com>
2018-10-27 20:13:08 +0200

    Upd

---
+++ Java Syntax/Level04/task0439/src/task0439/Solution.java
@@ -0,0 +1,15 @@
+package task0439;
+import java.io.*;
+
+public class Solution {
+	
+    public static void main(String[] args) throws Exception {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	String s = br.readLine();
+    	
+    	for (int i = 0; i < 10; i++) {
+    		System.out.println(s + " " + "loves me.");
+    	}
+    }
+}
