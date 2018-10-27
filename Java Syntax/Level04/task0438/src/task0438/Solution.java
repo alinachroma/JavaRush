a72cee7cbbc6acf9440c1e2c8ac2b20d97d02a06
alinachroma <alinachroma@gmail.com>
2018-10-27 19:56:51 +0200

    Upd

---
+++ Java Syntax/Level04/task0438/src/task0438/Solution.java
@@ -0,0 +1,21 @@
+package task0438;
+
+/* 
+Drawing lines
+*/
+
+public class Solution {
+	
+	public static void main(String[] args) throws Exception {
+		
+        for (int x = 0; x < 9; x++) {
+        	 System.out.print('8');
+        	}
+        
+        for (int y = 0; y < 11; y++) {
+        	System.out.println('8');
+        }
+    }
+}
+
+
