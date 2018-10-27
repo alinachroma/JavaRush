3399b3ff8d6467ff5397782b1115bba66d7076ec
alinachroma <alinachroma@gmail.com>
2018-10-27 19:22:54 +0200

    Upd

---
+++ Java Syntax/Level04/task0437/src/task0437/Solution.java
@@ -0,0 +1,19 @@
+package task0437;
+
+/* 
+Triangle
+*/
+
+public class Solution {
+	
+    public static void main(String[] args) throws Exception{
+    	
+    	for (int x = 1; x < 10; x++) {
+    		for (int y = 0; y < x; y = y + 1) {
+    			System.out.print(y);
+    		}
+    		System.out.println();
+    	}
+    }
+}
+  
